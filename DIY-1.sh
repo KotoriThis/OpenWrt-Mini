33c083d33ec208c587c4a7c8b0c461b59e3dd3b9
KotoriThis <65011307+KotoriThis@users.noreply.github.com>
2024-04-23 18:01:50 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -17,3 +17,9 @@ sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.con
 # 5-添加PassWall插件
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+
+# 5-添加PassWall2插件
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+
+# 5-添加PassWall插件
+echo "src-git istore https://github.com/linkease/istore;main" >> "feeds.conf.default"
